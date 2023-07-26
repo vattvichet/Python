b4a0f77260e4212300f3eaa3768c06068ff7d153
vichet vatt <vichetvattedu@gmail.com>
2023-07-26 20:02:11 +0700

    math with condition

---
+++ pythonMath.py
@@ -0,0 +1,15 @@
+import math
+
+#/
+print("Enter a and b for (ax + b = 0)")
+a = int(input(" a :"))
+b = int(input(" b :"))
+if(a == 0):
+    print("Np root")
+else:
+    x = (-b)/a
+print("x = ", end= ' ')
+print(x)
+
+
+
