f4fadd08dcb49a734afb3e0546eb0d15e78ea906
vichet vatt <vichetvattedu@gmail.com>
2023-07-26 19:49:30 +0700

    first commit

---
+++ phytonInput.py
@@ -0,0 +1,4 @@
+a = int(input("input a :"))
+b = int(input("input b :"))
+s = a+b
+print(s)
---
+++ test1.py
@@ -0,0 +1,10 @@
+st = "Hello"
+name =  'vichet'
+print(st, end=' ') #Used for avoiding next line
+print(name)
+
+a,b,c = 10,20,30
+
+print(a)
+print(b)
+print(c)
