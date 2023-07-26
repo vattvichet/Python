78823dac630790a6b90c44c26d7122c5fc5cf52c
vichet vatt <vichetvattedu@gmail.com>
2023-07-26 20:18:48 +0700

    Updated math

--- pythonMath.py
+++ pythonMath.py
@@ -1,15 +1,30 @@
 import math
 
 #/
-print("Enter a and b for (ax + b = 0)")
+#print("Enter a and b for (ax + b = 0)")
+#a = int(input(" a :"))
+#b = int(input(" b :"))
+#if(a == 0):
+#    print("Np root")
+#else:
+ #   x = (-b)/a
+#print("x = ", end= ' ')
+#print(x)
+#print(math.sqrt(x))
+
+print("Enter a and b for (ax^2 + bx + c = 0)")
 a = int(input(" a :"))
 b = int(input(" b :"))
-if(a == 0):
-    print("Np root")
-else:
-    x = (-b)/a
+c = int(input(" c :"))
+delta = (b**2) - (4*a*c)
 print("x = ", end= ' ')
-print(x)
-
+if(delta < 0):
+    print("No root")
+if(delta == 0):
+    print((-b)/(2*b))
+if(delta > 0):
+    print(((-b + math.sqrt(delta))/ 2*a))
+    print(((-b - math.sqrt(delta))/ 2*a))
+    
 
 
